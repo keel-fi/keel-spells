dbc80e9c2cb85b685b7d6c1e1a86243107af6f0a
Matt Auer <mattauer@umich.edu>
2025-11-18 09:54:17 -0600

    refactor: move into individual functions and link forum post

--- src/proposals/20251127/KeelEthereum_20251127.sol
+++ src/proposals/20251127/KeelEthereum_20251127.sol
@@ -28,28 +28,59 @@ contract KeelEthereum_20251127 is KeelPayloadEthereum {
     uint256 internal constant TRANSFER_SLOPE_E18 = 50_000_000e18 / uint256(1 days);
 
     function _execute() internal override {
-        _changeRateLimits();
-        _setRecipients();
+        // [Ethereum] Keel - Set rate limit for USDC to USDS
+        // Forum : https://forum.sky.money/t/november-27-2025-prime-technical-scope-parameter-change-for-upcoming-spell/27406#:~:text=%5BMainnet%5D%20LIMIT_USDS_TO_USDC
+        _changeUSDSToUSDCRateLimits();
+
+        // [Ethereum] Keel - Set rate limit for USDS to sUSDS
+        // Forum : https://forum.sky.money/t/november-27-2025-prime-technical-scope-parameter-change-for-upcoming-spell/27406#:~:text=%5BMainnet%5D%20LIMIT_4626_DEPOSIT
+        _changeUSDStoSUSDSRateLimits();
+
+        // [Ethereum] Keel - Set rate limit for USDC to CCTP General
+        // Forum : https://forum.sky.money/t/november-27-2025-prime-technical-scope-parameter-change-for-upcoming-spell/27406#:~:text=%5BMainnet%5D%20LIMIT_4626_WITHDRAW
+        _changeUSDCtoCCTPGeneralRateLimits();
+
+        // [Ethereum] Keel - Set rate limit for USDC to CCTP Solana
+        // Forum : https://forum.sky.money/t/november-27-2025-prime-technical-scope-parameter-change-for-upcoming-spell/27406#:~:text=%5BMainnet%5D%20LIMIT_USDC_TO_DOMAIN
+        _changeUSDCtoCCTPSolanaRateLimits();
+
+        // [Ethereum] Keel - Set rate limit for USDS to LayerZero Solana
+        // Forum : https://forum.sky.money/t/november-27-2025-prime-technical-scope-parameter-change-for-upcoming-spell/27406#:~:text=%5BMainnet%5D%20LIMIT_LAYERZERO_TRANSFER
+        _changeUSDStoLayerZeroSolanaRateLimits();
+
+        // [Ethereum] Keel - Set CCTP Mint Recipient
+        // Forum : https://forum.sky.money/t/november-27-2025-prime-technical-scope-parameter-change-for-upcoming-spell/27406#:~:text=%5BMainnet%5D%20mintRecipients
+        _setCCTPMintRecipient();
+
+        // [Ethereum] Keel - Set LayerZero Mint Recipient
+        // Forum : https://forum.sky.money/t/november-27-2025-prime-technical-scope-parameter-change-for-upcoming-spell/27406#:~:text=%5BMainnet%5D%20layerZeroRecipients
+        _setLayerZeroMintRecipient();
     }
 
-    function _changeRateLimits() internal {
+    function _changeUSDSToUSDCRateLimits() internal {
         // ---------- [Mainnet] Update USDS to USDC Rate Limits ----------
         // BEFORE :      10,000 max ;       5,000/day slope
         // AFTER  : 100,000,000 max ;  50,000,000/day slope
         _setUSDSToUSDCRateLimit(TRANSFER_LIMIT_E6, TRANSFER_SLOPE_E6);
+    }
 
+    function _changeUSDStoSUSDSRateLimits() internal {
         // ---------- [Mainnet] Update USDS to sUSDS Rate Limits ----------
         // BEFORE :      10,000 max ;       5,000/day slope
         // AFTER  : 100,000,000 max ;  50,000,000/day slope
         _onboardERC4626Vault(Ethereum.SUSDS, TRANSFER_LIMIT_E18, TRANSFER_SLOPE_E18);
+    }
 
+    function _changeUSDCtoCCTPGeneralRateLimits() internal {
         // ---------- [Mainnet] Update USDC to CCTP General Rate Limits ----------
         // BEFORE :           0 max ;           0 slope
         // AFTER  : 100,000,000 max ;  50,000,000/day slope
         bytes32 generalCctpKey = MainnetController(Ethereum.ALM_CONTROLLER).LIMIT_USDC_TO_CCTP();
         IRateLimits(Ethereum.ALM_RATE_LIMITS)
             .setRateLimitData(generalCctpKey, TRANSFER_LIMIT_E6, TRANSFER_SLOPE_E6);
+    }
 
+    function _changeUSDCtoCCTPSolanaRateLimits() internal {
         // ---------- [Mainnet] Update USDC to CCTP Solana Rate Limits ----------
         // BEFORE :           0 max ;           0 slope
         // AFTER  : 100,000,000 max ;  50,000,000/day slope
@@ -58,7 +89,9 @@ contract KeelEthereum_20251127 is KeelPayloadEthereum {
         );
         IRateLimits(Ethereum.ALM_RATE_LIMITS)
             .setRateLimitData(solanaCctpKey, TRANSFER_LIMIT_E6, TRANSFER_SLOPE_E6);
+    }
 
+    function _changeUSDStoLayerZeroSolanaRateLimits() internal {
         // ---------- [Mainnet] Update USDS to LayerZero Solana Rate Limits ----------
         // BEFORE :           0 max ;           0 slope
         // AFTER  : 100,000,000 max ;  50,000,000/day slope
@@ -73,13 +106,15 @@ contract KeelEthereum_20251127 is KeelPayloadEthereum {
             .setRateLimitData(solanaLayerZeroKey, TRANSFER_LIMIT_E18, TRANSFER_SLOPE_E18);
     }
 
-    function _setRecipients() internal {
+    function _setCCTPMintRecipient() internal {
         // ---------- [Mainnet] Update CCTP Mint Recipient ----------
         // BEFORE : 0
         // AFTER  : KEEL_SVM_ALM_CONTROLLER_AUTHORITY
         MainnetController(Ethereum.ALM_CONTROLLER)
             .setMintRecipient(CCTPForwarder.DOMAIN_ID_CIRCLE_SOLANA, Ethereum.KEEL_SVM_ALM_CONTROLLER_AUTHORITY);
+    }
 
+    function _setLayerZeroMintRecipient() internal {
         // ---------- [Mainnet] Update LayerZero Mint Recipient ----------
         // BEFORE : 0
         // AFTER  : KEEL_SVM_ALM_CONTROLLER_AUTHORITY
