b57ec7f5123b428177cbab1ebcb0e7e2057b82ec
Matt Auer <mattauer@umich.edu>
2025-12-12 13:01:53 -0600

    feat: cross chain paylaod

--- src/proposals/20260115/KeelEthereum_20260115.sol
+++ src/proposals/20260115/KeelEthereum_20260115.sol
@@ -2,6 +2,7 @@
 pragma solidity 0.8.25;
 
 import {CCTPForwarder} from "lib/xchain-helpers/src/forwarders/CCTPForwarder.sol";
+import { LZForwarder, ILayerZeroEndpointV2 } from "lib/xchain-helpers/src/forwarders/LZForwarder.sol";
 
 import {Ethereum, KeelPayloadEthereum} from "src/libraries/KeelPayloadEthereum.sol";
 
@@ -9,6 +10,7 @@ import {IRateLimits} from "lib/keel-alm-controller/src/interfaces/IRateLimits.so
 
 import {RateLimitHelpers} from "lib/keel-alm-controller/src/RateLimitHelpers.sol";
 import {MainnetController} from "lib/keel-alm-controller/src/MainnetController.sol";
+
 
 /**
  * @title  January 15, 2026 Keel Ethereum Proposal
@@ -18,6 +20,8 @@ import {MainnetController} from "lib/keel-alm-controller/src/MainnetController.s
  * Vote:  TODO
  */
 contract KeelEthereum_20260115 is KeelPayloadEthereum {
+    uint32 internal constant ENDPOINT_ID_SOLANA = 30168;
+
     function _execute() internal override {
         // [Ethereum] Keel - Update CCTP Recipient
         // Forum: TODO
@@ -25,20 +29,41 @@ contract KeelEthereum_20260115 is KeelPayloadEthereum {
 
         // [Ethereum] Keel - Execute cross-chain payload
         // Forum: TODO
-        _executeCrossChainPayload();
+        _executeAddNewRelayerCrossChainPayload();
     }
 
     function _setCCTPMintRecipient() internal {
         // ---------- [Mainnet] Update CCTP Mint Recipient ----------
         // BEFORE : 0
         // AFTER  : TODO
-        MainnetController(Ethereum.ALM_CONTROLLER)
-            .setMintRecipient(CCTPForwarder.DOMAIN_ID_CIRCLE_SOLANA, "TODO");
+        MainnetController(Ethereum.ALM_CONTROLLER).setMintRecipient(CCTPForwarder.DOMAIN_ID_CIRCLE_SOLANA, "TODO");
     }
 
-    function _executeCrossChainPayload() internal {
-        // ---------- [Ethereum] Execute cross-chain payload ----------
+    function _executeAddNewRelayerCrossChainPayload() internal {
+        // ---------- [Ethereum] Execute Add New Relayer Cross-Chain Payload ----------
         // Forum: TODO
-        // TODO: Implement cross-chain payload execution
+        LZForwarder.sendMessage({
+            _dstEid: ENDPOINT_ID_SOLANA,
+            _receiver: Ethereum.KEEL_SVM_ALM_CONTROLLER_AUTHORITY,
+            endpoint: ILayerZeroEndpointV2(LZForwarder.ENDPOINT_ETHEREUM),
+            _message: bytes("TODO"),
+            _options: bytes("TODO"),
+            _refundAddress: Ethereum.KEEL_PROXY,
+            _payInLzToken: false
+        });
+    }
+
+    function _executeUpgradeALMControllerCrossChainPayload() internal {
+        // ---------- [Ethereum] Execute Upgrade ALM Controller Cross-Chain Payload ----------
+        // Forum: TODO
+        LZForwarder.sendMessage({
+            _dstEid: ENDPOINT_ID_SOLANA,
+            _receiver: Ethereum.KEEL_SVM_ALM_CONTROLLER_AUTHORITY,
+            endpoint: ILayerZeroEndpointV2(LZForwarder.ENDPOINT_ETHEREUM),
+            _message: bytes("TODO"),
+            _options: bytes("TODO"),
+            _refundAddress: Ethereum.KEEL_PROXY,
+            _payInLzToken: false
+        });
     }
 }
