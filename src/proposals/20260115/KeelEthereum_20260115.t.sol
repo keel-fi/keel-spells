6d99d9773810197ea546c03c6944e123af361dc7
Matt Auer <mattauer@umich.edu>
2026-01-08 09:27:29 -0600

    test: transfer of usdc

--- src/proposals/20260115/KeelEthereum_20260115.t.sol
+++ src/proposals/20260115/KeelEthereum_20260115.t.sol
@@ -5,9 +5,11 @@ import "src/test-harness/KeelTestBase.sol";
 
 import {CCTPForwarder} from "lib/xchain-helpers/src/forwarders/CCTPForwarder.sol";
 import {MainnetController} from "lib/keel-alm-controller/src/MainnetController.sol";
-
+import {RateLimitHelpers} from "lib/keel-alm-controller/src/RateLimitHelpers.sol";
+import { IRateLimits } from "lib/keel-alm-controller/src/interfaces/IRateLimits.sol";
 import {Ethereum} from "lib/keel-address-registry/src/Ethereum.sol";
 import {ChainIdUtils} from "src/libraries/ChainId.sol";
+
 
 contract KeelEthereum_20260115Test is KeelTestBase {
     MainnetController internal controller = MainnetController(Ethereum.ALM_CONTROLLER);
@@ -35,5 +37,32 @@ contract KeelEthereum_20260115Test is KeelTestBase {
         // Check that the CCTP mint recipient was updated (not zero)
         bytes32 recipient = controller.mintRecipients(CCTPForwarder.DOMAIN_ID_CIRCLE_SOLANA);
         assertEq(recipient, Ethereum.KEEL_SVM_ALM_CONTROLLER_AUTHORITY_USDC_ATA, "cctp-mint-recipient-not-updated");
+
+        uint256 usdcAmount = 10_000e6;
+        uint256 usdcToCctpRateLimit = 100_000_000e6;
+        uint256 solanaRateLimit = 100_000_000e6;
+
+        bytes32 solanaKey = RateLimitHelpers.makeDomainKey(
+            MainnetController(Ethereum.ALM_CONTROLLER).LIMIT_USDC_TO_DOMAIN(), CCTPForwarder.DOMAIN_ID_CIRCLE_SOLANA
+        );
+
+        IRateLimits rateLimits = IRateLimits(Ethereum.ALM_RATE_LIMITS);
+
+        assertEq(rateLimits.getCurrentRateLimit(controller.LIMIT_USDC_TO_CCTP()), usdcToCctpRateLimit);
+        assertEq(rateLimits.getCurrentRateLimit(solanaKey), solanaRateLimit);
+
+        vm.startPrank(Ethereum.ALM_RELAYER);
+        controller.mintUSDS(usdcAmount * 1e12);
+        controller.swapUSDSToUSDC(usdcAmount);
+
+        // Transferring more than ratelimit fails
+        vm.expectRevert("RateLimits/rate-limit-exceeded");
+        controller.transferUSDCToCCTP(usdcToCctpRateLimit + 1, CCTPForwarder.DOMAIN_ID_CIRCLE_SOLANA);
+
+        controller.transferUSDCToCCTP(usdcAmount, CCTPForwarder.DOMAIN_ID_CIRCLE_SOLANA);
+        vm.stopPrank();
+
+        assertEq(rateLimits.getCurrentRateLimit(controller.LIMIT_USDC_TO_CCTP()), usdcToCctpRateLimit - usdcAmount);
+        assertEq(rateLimits.getCurrentRateLimit(solanaKey), solanaRateLimit - usdcAmount);
     }
 }
